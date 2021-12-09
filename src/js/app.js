1922fe115d6aa4f7011c5b59d530c77d6fe863fe
wujunze <itwujunze@163.com>
2021-12-09 14:50:16 +0800

    feat: handleAdopt

--- src/js/app.js
+++ src/js/app.js
@@ -102,9 +102,30 @@ App = {
 
     var petId = parseInt($(event.target).data("id"));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    // 获取用户账号
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed()
+        .then(function (instance) {
+          adoptionInstance = instance;
+
+          // 发送交易领养宠物
+          return adoptionInstance.adopt(petId, { from: account });
+        })
+        .then(function (result) {
+          return App.markAdopted();
+        })
+        .catch(function (err) {
+          console.log(err.message);
+        });
+    });
   },
 };
 
